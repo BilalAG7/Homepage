99f639cf8012030c7f91aef8be736687ade750b6
BilalAG7 <90452010+BilalAG7@users.noreply.github.com>
2023-03-17 14:43:28 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Bilal',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: 'f7b9795af021d5b7e584a0f4fae9ce59', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '25.204849',
+	defaultLongitude: '55.270782',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+      			name: 'Twitter',
+      			icon: 'twitter',
+      			link: 'https://www.twitter.com/home',
+		},
+		{
+			id: '2',
+      			name: 'AniList',
+      			icon: 'list',
+      			link: 'https://anilist.co/home',
+		},
+		{
+			id: '3',
+      			name: 'Mail',
+      			icon: 'mail',
+      			link: 'https://mail.google.com/mail/u/0/#inbox',
+		},
+		{
+			id: '4',
+      			name: 'YouTube',
+      			icon: 'youtube',
+      			link: 'https://youtube.com/',
+		},
+		{
+			id: '5',
+      			name: 'Official Mail',
+      			icon: 'inbox',
+      			link: 'https://mail.google.com/mail/u/1/#inbox',
+		},
+		{
+			id: '6',
+      			name: 'Cubari Proxy',
+      			icon: 'book-open',
+      			link: 'https://proxy.cubari.moe/#/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'home',
+			id: '1',
+			links: [
+				{
+					name: 'Netflix',
+        				link: 'https://www.netflix.com/browse',
+				},
+				{
+					name: 'Prime Video',
+        				link: 'https://www.primevideo.com/region/eu/',
+				},
+				{
+					name: 'Reddit',
+        				link: 'https://www.reddit.com/',
+				},
+				{
+					name: 'EDI Student Console',
+        				link: 'https://student.edi-uae.com/Student/Student/StudentMenu.aspx',
+				},
+			],
+		},
+		{
+			icon: 'heart',
+			id: '2',
+			links: [
+				{
+					name: 'Letterboxd',
+        				link: 'https://letterboxd.com/',
+				},
+				{
+					name: 'Stats.fm',
+        				link: 'https://preview.stats.fm/bilalag',
+				},
+				{
+					name: 'RARBG',
+        				link: 'https://rarbgmirror.org/index70.php',
+				},
+				{
+					name: 'Nyaa',
+        				link: 'https://nyaa.si/',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
